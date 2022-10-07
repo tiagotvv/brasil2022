fd6d7ecd61dc424d0149237779d067dfd6501faa
Tiago Vinhoza <tiago.vinhoza@gmail.com>
2022-10-07 13:06:06 +0100

    add resumo subsection

--- simulador.py
+++ simulador.py
@@ -24,7 +24,7 @@ st.subheader('Selecione o nível de mobilização do eleitor')
 
 col1, col2, col3 = st.columns(3)
 cp =  col1.slider('Eleitores dos candidatos derrotados no 1o.turno que vão dar votos válidos no 2o.turno (%)', 70, 95, 85)
-cpbna=  col3.slider('Eleitores que se abstiveram ou votaram branco/nulo no 1o.turno que vão dar votos válidos no 2o.turno (%)', 0, 25, 12)
+cpbna=  col3.slider('Eleitores que se abstiveram ou votaram branco/nulo no 1o.turno que vão dar votos válidos no 2o.turno (%)', 0, 30, 12)
 cp = cp/100
 cpbna = cpbna/100
 
@@ -44,6 +44,7 @@ OutrosLula = ol/100
 BNALula = bl/100
 eleitores = df.sum()
 df.loc['Outros'] = df.loc['Padre Kelmon'] + df.loc['Leo Pericles'] +df.loc['Sofia Manzano'] + df.loc['Vera Lucia'] + df.loc['Eymael']  
+df.loc['Derrotados'] = df.loc['Ciro'] + df.loc['Tebet'] +df.loc['dAvila'] + df.loc['Soraya'] + df.loc['Outros']  
 df.loc['BNA'] = df.loc['Brancos'] + df.loc['Nulos'] +df.loc['Abstencoes']
 
 Lula2T = int(df.loc['Lula'] * 0.98*0.98 ) + int(df.loc['Bolsonaro'] * 0.98*0.01 )+ int(df.loc['Ciro'] * cp * CiroLula )+ int(df.loc['Tebet'] * cp * TebetLula )+ int(df.loc['Soraya'] * cp * SorayaLula )+ int(df.loc['dAvila'] * cp * DavilaLula) + int(df.loc['Outros'] * cp * OutrosLula )+ int(df.loc['BNA'] * cpbna * BNALula )
@@ -59,8 +60,12 @@ col1.metric('Lula',str("{:,.0f}".format(Lula2T)))
 col3.metric('Bolsonaro', str("{:,.0f}".format(Bolsonaro2T)))
 col1.metric('% votos válidos',str("{:,.2f}".format( round(100*Lula2T/(Lula2T+Bolsonaro2T),2))+"%"))
 col3.metric('% votos válidos', str("{:,.2f}".format(round(100*Bolsonaro2T/(Lula2T+Bolsonaro2T),2))+"%"))
+st.markdown('#### Resumo')
 st.write('Taxa aproximada de comparecimento: '+ str(round(100*((Lula2T+Bolsonaro2T)/0.96/eleitores.values[0]),1))+"%")
 st.write('Total de votos válidos: '+ str("{:,.0f}".format(Bolsonaro2T+Lula2T)))
+st.write('Votantes do 1ºturno que deram votos válidos no 2ºturno: '+ str("{:,.0f}".format(Bolsonaro2T+Lula2T-int(df.loc['BNA'] * cpbna))))
+st.write('Não votantes do 1ºturno que deram votos válidos no 2ºturno: '+ str("{:,.0f}".format(int(df.loc['BNA'] * cpbna)) ))
+
 
 with st.expander('Notas:'):
     st.write('Diferentes cenários de transferências de votos. \
